a33b9f7c818620596b6e667aa90fb9fcf582c3ad
Jane Wen <janenxiao@outlook.com>
2020-02-02 18:59:21 -0500

    hard resize window first time map is on

--- websock_py.py
+++ websock_py.py
@@ -3,19 +3,19 @@ import time
 import json
 
 # data = {'result': 2, 'scale': 2}
-data = {'result': None, 'scale': 2, 'x': 0.1, 'y': 0.25}
+data = {'result': 5, 'scale': 2, 'x': 0.1, 'y': 0.25}
 
 from websocket import create_connection
 ws = create_connection("ws://localhost:8080/gesture")
-# ws = create_connection("ws://localhost:8080/handpoint")
 startTime = time.time()
 start = datetime.now()
 print('start second:{}.{}'.format(start.second, start.microsecond))
 # print("Sending json string...")
-for _ in range(10):
-    ws.send(json.dumps(data))
-    data["x"] = data["x"] + 0.01
-    time.sleep(0.1)
+# for _ in range(10):
+#     ws.send(json.dumps(data))
+#     data["x"] = data["x"] + 0.01
+#     time.sleep(0.1)
+ws.send(json.dumps(data))
 print("Sent")
 
 # print("Receiving...")
