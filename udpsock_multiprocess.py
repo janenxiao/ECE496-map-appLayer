241167fa0cc8540adcdcef99a922df51cb9e1e67
Jane Wen <janenxiao@outlook.com>
2020-05-02 18:51:13 -0400

    add cell name, elapsed time measure

--- udpsock_multiprocess.py
+++ udpsock_multiprocess.py
@@ -1,26 +1,35 @@
 import json
 import time
+from datetime import datetime
 import multiprocessing
 
+from websocket import create_connection
 import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 8080
 
 def send_to_mapapp(data):
+    # ws = create_connection("ws://localhost:8080/gesture")
+    # ws.send(json.dumps(data))
+    # ws.close()
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, UDP
     sock.sendto(json.dumps(data).encode(), (UDP_IP, UDP_PORT))
 
-#ws = create_connection("ws://localhost:8080/gesture")
+
 if __name__ == '__main__':    
     
     # data = {'result': 2, 'scale': 2}
-    data = {'result': None, 'scale': 2, 'x': None, 'y': None, 'r': 0.12}
+    data = {'result': 2, 'scale': -2, 'x': None, 'y': None, 'r': 0.15}
 
     processes = []
-    print("Start...")
+
+    start = datetime.now()
+    print('start second:{}.{}'.format(start.second, start.microsecond))
+
     startTime = time.time()
-    for _ in range(5):
+    for _ in range(1):
         
         p = multiprocessing.Process(target=send_to_mapapp, args=[data])
         p.start()
